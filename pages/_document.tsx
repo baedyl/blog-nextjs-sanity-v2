4614ab7776c9dd7e9009a727ca7d417c49851093
Emmanuel <emmanuelabona@gmail.com>
2023-08-18 09:29:35 +0100

    Add google ad sense script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8055354502682017"
+          crossOrigin="anonymous"></script>
+      </Head>
       <body className="bg-white text-black">
         <Main />
         <NextScript />
